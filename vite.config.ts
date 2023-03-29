b559013e1a7312ea020f7a7d8babdcdc111710ef
macjjuni <macjjuni@gmail.com>
2023-03-29 16:21:47 +0900

    fix: alias 설정 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import path from 'path'
+import { fileURLToPath, URL } from 'url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), tsconfigPaths()],
   resolve: {
-    alias: {
-      '@': path.join(__dirname, 'src/'),
-    },
+    alias: [{ find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) }],
   },
 })
